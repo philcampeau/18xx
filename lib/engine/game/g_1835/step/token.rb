2754d08f0bead0293638b6e5c5b8231b9acfd875
kepakiano <schmitz.basti@googlemail.com>
2026-08-10 22:33:51 +0200

    [1835] implement distance based tokening cost

---
+++ lib/engine/game/g_1835/step/token.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Engine
+  module Game
+    module G1835
+      module Step
+        class Token < Engine::Step::Token
+          def adjust_token_price_ability!(_entity, token, hex, _city, special_ability: nil)
+            token.price = token_price(token, hex)
+            [token, nil]
+          end
+
+          def token_price(token, hex)
+            home_hex = @game.hex_by_id(token.corporation.coordinates)
+            token_cost = home_hex.distance(hex) * token.price
+            return token_cost unless bayern_tokens_dresden(hex, token)
+
+            # token cost is "as the crow flies", but the crow is not allowed to leave the board,
+            # so it has to take a detour from München to Dresden. Since this is the only edge
+            # case, it's much easier to implement it hard-coded than to have logic that checks
+            # for the board boundaries
+            token_cost + 20
+          end
+
+          def bayern_tokens_dresden(hex, token)
+            hex.coordinates == 'H20' && token.corporation == @game.corporation_by_id('BY')
+          end
+        end
+      end
+    end
+  end
+end
