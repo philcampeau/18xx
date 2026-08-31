d883a15990533f7c3921d99928c4e2ced3b37015
Sebastian Schmitz <schmitz.basti@googlemail.com>
2026-08-31 20:24:57 +0200

    [1835] skip token step if a corporation cannot afford to place a token

--- lib/engine/game/g_1835/step/token.rb
+++ lib/engine/game/g_1835/step/token.rb
@@ -5,6 +5,25 @@ module Engine
     module G1835
       module Step
         class Token < Engine::Step::Token
+          def auto_actions(entity)
+            [Engine::Action::Pass.new(entity)] unless can_place_token?(entity)
+          end
+
+          def can_place_token?(entity)
+            # Cheaper to do the graph first, then check affordability
+            current_entity == entity &&
+              !(token = entity.next_token).nil? &&
+              @game.token_graph_for_entity(entity).can_token?(entity) &&
+              can_afford_token?(token, buying_power(entity))
+          end
+
+          def can_afford_token?(token, cash)
+            corp = token.corporation
+            @game.token_graph_for_entity(corp).tokenable_cities(corp).any? do |city|
+              token_price(token, city.tile.hex) <= cash
+            end
+          end
+
           def adjust_token_price_ability!(_entity, token, hex, _city, special_ability: nil)
             token.price = token_price(token, hex)
             [token, nil]
